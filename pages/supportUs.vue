3397640cedba1cf8485d301f80e96c4b729b9196
treeleaves30760 <treeleaves30760@gmail.com>
2022-03-30 21:41:57 +0800

    fix: finish the optimal of coding style

--- pages/supportUs.vue
+++ pages/supportUs.vue
@@ -1,30 +1,33 @@
 <template>
-  <v-container class="px-6 px-md-0">
+  <v-container class="">
     <v-row>
-      <v-col cols="12" md="7">
-        <v-img
-          src="https://eecampapi.nthuee.org/public/9fd9216f-ab48-48b7-85a6-66ab17fdc27a_NTHUEECamp.jpg"
-        ></v-img>
+      <v-col cols="12" md="8" class="d-flex align-center">
+        <v-img src="https://i.imgur.com/qF7zbDK.jpg"></v-img>
       </v-col>
       <v-col cols="12" md="4">
-        <h1 class="text-center">支持我們</h1>
+        <div class="text-Heading42">支持我們</div>
         <p class="font-weight-bold p-2">
           我們相信舉辦電機營，能讓參與的高中生對電機領域有更深一層的理解，藉由七天六夜的營期，結合許多大地活動與知識教學，讓學員在享受快樂的同時能吸收知識，豐富他們的暑假。如果您對於讓高中生探索電機領域有興趣，或是希望能藉由贊助我們讓我們能舉辦更好的活動，歡迎您以各種方式支持我們。
         </p>
-        <div class="text-center">
-          <v-alert text border="bottom" color="success">
-            <h3>粉專信箱</h3>
-            <h5>officialnthueecamp@gmail.com</h5>
-          </v-alert>
-          <v-alert text border="bottom" color="warning">
-            <h3>粉絲專頁</h3>
-            <h5>
-              <a href="https://www.facebook.com/NTHUEECAMP/" class="muted">
-                https://www.facebook.com/NTHUEECAMP/
-              </a>
-            </h5>
-          </v-alert>
-        </div>
+        <v-row justify="space-around">
+          <v-btn
+            x-large
+            large
+            href="mailto:mailto:officialnthueecamp@gmail.com"
+            class="text-Heading24 ma-2"
+          >
+            粉專信箱
+          </v-btn>
+          <v-btn
+            x-large
+            large
+            color="primary"
+            href="https://www.facebook.com/NTHUEECAMP/"
+            class="text-Heading24 ma-2"
+          >
+            粉絲專頁
+          </v-btn>
+        </v-row>
       </v-col>
       <!-- <v-col cols="12">
         <v-row>
